a260907ad94446e246d87fec43993134f76105cf
TheSunspot <1092312830@qq.com>
2021-10-12 22:59:07 +0800

    登录验证

---
+++ -- SQLite.sql
@@ -0,0 +1,13 @@
+-- SQLite
+INSERT INTO movie VALUES(1,'a','b');
+INSERT INTO movie VALUES(2,'aa','ab');
+INSERT INTO movie VALUES(3,'aaa','ba');
+INSERT INTO movie VALUES(4,'aa','asdb');
+INSERT INTO movie VALUES(5,'aaa','sdvbvds');
+INSERT INTO movie VALUES(6,'aaaa','bcasc');
+INSERT INTO movie VALUES(7,'aaaaaa','xasb');
+INSERT INTO movie VALUES(8,'aaasa','azb');
+INSERT INTO movie VALUES(9,'asd','bzz');
+INSERT INTO movie VALUES(10,'aasdasd','zb');
+INSERT INTO movie VALUES(11,'aasf','bddsa');
+INSERT INTO movie VALUES(12,'azxc','casb');
